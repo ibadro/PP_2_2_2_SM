efc0f9682a8727d7a433b1348eb82edb00351879
Ilnur Badro <101541200+ibadro@users.noreply.github.com>
2022-10-12 15:03:18 +0500

    !

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,27 +1,33 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import web.service.CarService;
 
 @Controller
-@RequestMapping("/cars")                  // ТЗ: создайте еще один контроллер, замаппленный на /cars
+@RequestMapping("/cars")
 public class CarController {
     private final CarService carService;
 
-    @Autowired                            // внедряем зависимость через конструктор
+    @Autowired
     public CarController(CarService carService) {
         this.carService = carService;
     }
 
     @GetMapping()
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        model.addAttribute("cars", carService.getCars(count));
+        if (count < 0){
+            System.out.println(" number is positive ");
+        } else { model.addAttribute("cars", carService.getCars(count));}
         return "cars";
     }
+
+
 }
